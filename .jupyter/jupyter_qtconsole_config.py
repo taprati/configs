e3f5c062b0baf2089f1a1462db3b6d134e2fee2e
taprati <tyleraprati@gmail.com>
2021-09-22 17:36:47 -0400

    Added jupyter configs for qtconsole

---
+++ .jupyter/jupyter_qtconsole_config.py
@@ -0,0 +1,13 @@
+#############################################
+# Configuration file for jupyter-qtconsole. #
+#############################################
+
+c.ConsoleWidget.include_other_output = True
+c.ConsoleWidget.other_output_prefix = '[vim]'
+
+c.JupyterWidget.console_height = 50
+c.JupyterWidget.console_width = 80
+c.JupyterWidget.font_family = 'Source Code Pro'
+c.JupyterWidget.font_size = 14
+c.JupyterWidget.scrollbar_visibility = True
+
